a75ceea41c3169cb4920f184127099a381474e95
Florian Sowade <f.sowade@suora.com>
2025-03-18 12:23:58 +0100

    Add a snapshot test with msw

--- src/user/UserTable.test.tsx
+++ src/user/UserTable.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { UserTable } from "./UserTable.tsx";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("<UserTable>", () => {
   test("renders without crashing", async () => {
@@ -9,6 +9,14 @@ describe("<UserTable>", () => {
 
   test("loading spinner matches snapshot", async () => {
     const result = render(<UserTable />);
+
+    expect(result.container).toMatchSnapshot();
+  });
+
+  test("rendered table matches snapshot", async () => {
+    const result = render(<UserTable />);
+
+    await screen.findByText("Chelsey Dietrich");
 
     expect(result.container).toMatchSnapshot();
   });
