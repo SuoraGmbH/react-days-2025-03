69ce1960cf2fc76674167da1ebe7e3e6215e3838
Florian Sowade <f.sowade@suora.com>
2025-03-18 12:48:18 +0100

    Add a user interaction snapshot test

--- src/user/UserTable.test.tsx
+++ src/user/UserTable.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "vitest";
 import { UserTable } from "./UserTable.tsx";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("<UserTable>", () => {
   test("renders without crashing", async () => {
@@ -17,6 +18,17 @@ describe("<UserTable>", () => {
     const result = render(<UserTable />);
 
     await screen.findByText("Chelsey Dietrich");
+
+    expect(result.container).toMatchSnapshot();
+  });
+
+  test("rendered table sorted descending matches snapshot", async () => {
+    const result = render(<UserTable />);
+
+    const user = userEvent.setup();
+
+    await screen.findByText("Chelsey Dietrich");
+    await user.click(screen.getByRole("button", { name: /sort by name/i }));
 
     expect(result.container).toMatchSnapshot();
   });
