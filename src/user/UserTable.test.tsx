ca4e9302c0c98bc165ca221a53e78a4cad596678
Florian Sowade <f.sowade@suora.com>
2025-03-18 12:07:16 +0100

    Add a snapshot test for the loading spinner

--- src/user/UserTable.test.tsx
+++ src/user/UserTable.test.tsx
@@ -1,9 +1,15 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { UserTable } from "./UserTable.tsx";
 import { render } from "@testing-library/react";
 
 describe("<UserTable>", () => {
   test("renders without crashing", async () => {
     render(<UserTable />);
+  });
+
+  test("loading spinner matches snapshot‰", async () => {
+    const result = render(<UserTable />);
+
+    expect(result.container).toMatchSnapshot();
   });
 });
